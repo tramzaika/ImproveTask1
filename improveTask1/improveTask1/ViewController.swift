13e5982344d1bb10692d030dddbaf2d526d59df0
lisa <tramzaika@gmail.com>
2021-03-15 19:39:42 +0700

    Create app

---
+++ improveTask1/improveTask1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  improveTask1
+//
+//  Created by 1234 on 15.03.2021.
+//  Copyright © 2021 Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
